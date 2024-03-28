e14e91d874e760bae2ee96560fd845542f28d95b
Hobar <huparelaa@eafit.edu.co>
2024-03-27 19:49:55 -0500

    ♻️ Refactor file upload and download functions in servicer.py

--- src/dataNode/filetransfer/servicer.py
+++ src/dataNode/filetransfer/servicer.py
@@ -2,8 +2,6 @@ import filetransfer_pb2
 import filetransfer_pb2_grpc
 import concurrent.futures
 import grpc
-import asyncio
-from grpc.experimental import aio
 
 import os
 from dotenv import load_dotenv
@@ -11,21 +9,31 @@ load_dotenv()
 
 class FileTransferServicer(filetransfer_pb2_grpc.FileTransferServicer):
     def UploadFile(self, request, context):
-        with open(f"./files/{request.name}", "wb") as f: 
-            f.write(request.content)
-        return filetransfer_pb2.UploadResponse(success=True)
-    
+        try:
+            folder_path = f"./files/{request.name}"
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            file_path = os.path.join(folder_path, request.name)
+            with open(file_path, "wb") as f: 
+                f.write(request.content)
+            return filetransfer_pb2.UploadResponse(success=True)
+        except Exception as e:
+            return filetransfer_pb2.UploadResponse(success=False, message=str(e))
+        
     def DownloadFile(self, request, context):
-        file_content = b''
-        with open(f"./files/{request.name}", 'rb') as f:
-            file_content = f.read()
-        return filetransfer_pb2.FileChunk(name=request.name, content=file_content)
+        try:
+            file_content = b''
+            with open(f"./files/{request.name}", 'rb') as f:
+                file_content = f.read()
+            return filetransfer_pb2.FileChunk(name=request.name, content=file_content)
+        except Exception as e:
+            return filetransfer_pb2.FileChunk(name=request.name, content=b'')
     
-async def grpcServer():
-    server = aio.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+def grpcServer():
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
     filetransfer_pb2_grpc.add_FileTransferServicer_to_server(FileTransferServicer(), server)
     port = os.getenv("GRPC_PORT")
     server.add_insecure_port(f"[::]:{port}")
-    await server.start()
+    server.start()
     print(f"Server gRPC started on port {port}")
-    await server.wait_for_termination()
+    server.wait_for_termination()
--- src/dataNode/server.py
+++ src/dataNode/server.py
@@ -1,24 +1,9 @@
-import asyncio
 from filetransfer.servicer import grpcServer
 from services.auth import login
-from api import heartbeat as heartbeat_app
-import uvicorn
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-async def run_uvicorn():
-    config = uvicorn.Config(heartbeat_app.app, host="0.0.0.0", port=int(os.getenv("HEARTBEAT_PORT")), loop="asyncio")
-    server = uvicorn.Server(config)
-    await server.serve()
-
-async def main():
-    print(login())
-    await asyncio.gather(
-        run_uvicorn(),
-        grpcServer(),
-    )
+def main():
+    #print(login()) <- wait for the login function to be implemented on NameNode
+    grpcServer()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
