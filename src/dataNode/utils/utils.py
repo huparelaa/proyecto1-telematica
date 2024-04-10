bcd6096496e1d61344b71d0bf4ddebc98cf49eb5
Hobar <huparelaa@eafit.edu.co>
2024-04-09 19:56:15 -0500

    🚀 Add MY_IP environment variable to .env file

--- src/dataNode/utils/utils.py
+++ src/dataNode/utils/utils.py
@@ -1,14 +1,17 @@
 import socket
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_my_ip():
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.connect(("8.8.8.8", 80))
-            ip = s.getsockname()[0]
-    except Exception as e:
-        print(f"Error obtaining IP address: {e}")
-        ip = None # Fallback to localhost
+    ip=os.getenv("MY_IP")
+    # try:
+    #     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    #         s.connect(("8.8.8.8", 80))
+    #         ip = s.getsockname()[0]
+    # except Exception as e:
+    #     print(f"Error obtaining IP address: {e}")
+    #     ip = None # Fallback to localhost
     return ip
 
 def list_dir(path):
